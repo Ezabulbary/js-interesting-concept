2eafbceb2aed0a9b70ed7c3790048bf3cc814fab
Ezabulbary <ezabul.bary@gmail.com>
2022-04-03 19:44:26 +0600

    double

---
+++ true-false.js
@@ -0,0 +1,29 @@
+/* 
+falsy
+false
+0
+empty string
+undefined
+null
+NaN
+
+-------------------
+
+truthy
+true
+any number (positive or negative)
+any string including single whitespace, '0', 'false'
+[]
+{}
+anything else that is not falsy will be truthy
+
+*/
+
+let x = 0;
+
+if(x){
+    console.log('variable is truthy')
+}
+else{
+    console.log('variable is falsy')
+}
